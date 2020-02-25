451303e0198d4d1a1367bab4a346da8920919aa4
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-25 20:50:46 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,8 @@
 class Owner
   attr_reader :name, :species
   
+  @@all = []
+  
   def initialize(name, species = "human")
     @name = name 
     @species = species
@@ -12,5 +14,6 @@ class Owner
   end 
   
   def save
-    @all 
+    @all << self 
+  end 
 end
