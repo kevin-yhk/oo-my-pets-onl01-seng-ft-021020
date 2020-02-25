cfb59e78a6a18b3215862f7f753bd32f03963184
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-25 20:50:25 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,11 +4,13 @@ class Owner
   def initialize(name, species = "human")
     @name = name 
     @species = species
+    save
   end 
   
   def say_species
     "I am a #{@species}."
   end 
   
-  
+  def save
+    @all 
 end
