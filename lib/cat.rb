9e9d23a1bd2e00be1c3365d2ec50927cf6ed7440
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-25 20:55:45 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,11 +2,18 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name 
   
+  @@all = []
+  
   def initialize(name, owner, mood = "nervous")
     @name = name 
     @owner = owner
     @mood = mood
+    save 
   end 
   
+  def save 
+    @@all << self 
+  end 
   
+  def
 end
